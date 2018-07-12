2476cb91afe54c5f5ebe753acc170d9b55761bab
atomheartother <atomheartother@gmail.com>
2018-07-12 22:29:41 +0100

    Added the visibility ELF attribute

--- src/exported.hh
+++ src/exported.hh
@@ -1,12 +1,24 @@
 #pragma once
 
 // Define EXPORTED for any platform
-#ifdef _WIN32
-# ifdef WIN_EXPORT
-#   define EXPORTED  __declspec( dllexport )
-# else
-#   define EXPORTED  __declspec( dllimport )
-# endif
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef WIN_EXPORT
+    #ifdef __GNUC__
+      #define EXPORTED __attribute__ ((dllexport))
+    #else
+      #define EXPORTED __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define EXPORTED __attribute__ ((dllimport))
+    #else
+      #define EXPORTED __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
 #else
-# define EXPORTED
+  #if __GNUC__ >= 4
+    #define EXPORTED __attribute__ ((visibility ("default")))
+  #else
+    #define EXPORTED
+  #endif
 #endif
