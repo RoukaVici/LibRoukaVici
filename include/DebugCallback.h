25d02efe0e39663bc84fb2d7bf7ddca0600bd712
atomheartother <atomheartother@gmail.com>
2018-09-21 21:48:49 +0200

    All windows pointers are __stdcalll

--- include/DebugCallback.h
+++ include/DebugCallback.h
@@ -3,8 +3,8 @@
 #ifdef _WIN32
     // This allows us to receive functions from Unity
     typedef void(__stdcall * UnityDebugCallback) (const char * str);
+    typedef void(__stdcall * DebugCallback) (const char * str);
 #else
     typedef void (*UnityDebugCallback)(const char* str);
+    typedef void (*DebugCallback)(const char* str);
 #endif
-
-typedef void (*DebugCallback)(const char* str);
