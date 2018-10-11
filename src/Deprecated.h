7fc6848fd8a4955ba504ecbb9a440b27b594d71a
atomheartother <atomheartother@gmail.com>
2018-10-11 09:48:45 +0200

    Removed UnityCallback internally, marked api as deprecated

--- src/Deprecated.h
+++
@@ -1,8 +0,0 @@
-#if defined(__GNUC__) || defined(__clang__)
-#   define DEPRECATED __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#   define DEPRECATED __declspec(deprecated)
-#else
-#   pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-#   define DEPRECATED
-#endif
